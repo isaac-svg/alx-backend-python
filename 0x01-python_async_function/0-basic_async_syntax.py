8826a5c89aabc646f56cda5a26879f1667693e76
isaac-svg <isaacsakyi009@gmail.com>
2024-05-06 12:25:41 +0000

    Anotatted function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
         Asynchronous function that awaits a result based on a delay
 
