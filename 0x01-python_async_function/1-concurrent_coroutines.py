9d1e60ca72dfca02a085cf89b674f4f5da14d08a
isaac-svg <isaacsakyi009@gmail.com>
2024-05-06 12:27:54 +0000

    Anotatted function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+
 
 async def wait_n(n: int, max_delay:int) -> List[float]:
     """
@@ -12,5 +12,6 @@ async def wait_n(n: int, max_delay:int) -> List[float]:
         n(int): Number of times to call wait_random
         max_delay(int): Maximum delay for each wait_random call
     """
+
     delays =  await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
     return sorted(delays)
