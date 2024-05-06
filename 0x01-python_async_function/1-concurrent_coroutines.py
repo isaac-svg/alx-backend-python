89f1edae5995cda82fdef20afdacc95d5d151937
isaac-svg <isaacsakyi009@gmail.com>
2024-05-06 12:51:36 +0000

    ADD documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
 
+
 async def wait_n(n: int, max_delay:int) -> List[float]:
     """
     Gathers  n number of calls of wait_random in a list and returns it as sorted
