b1a2c9f59ffe97f5e98a406ba17a7e468ee70217
isaac-svg <isaacsakyi009@gmail.com>
2024-05-07 10:14:37 +0000

    pycodestyle

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -12,12 +12,14 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-        Measures time taken for the wait_n to run n wait_random calls with maximum delay of max_delay
+        Measures time taken for the wait_n to run n
+        wait_random calls with maximum delay of
+        max_delay
         Args:
         n(int):
         number of times wait_n will call its internal function
         max_delay(int):
-        maximum delay fro each function call 
+        maximum delay fro each function call
     """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
