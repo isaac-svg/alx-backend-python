881ec9ea05532e2df5a33253cfad433c988c5fe6
isaac-svg <isaacsakyi009@gmail.com>
2024-05-07 10:23:22 +0000

    pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,5 +9,8 @@ wait_random = __import__('1-concurrent_coroutines').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Creates an asynchronous task
+    """
     coroutine: Coroutine = wait_random(max_delay)
     return asyncio.create_task(coroutine)
