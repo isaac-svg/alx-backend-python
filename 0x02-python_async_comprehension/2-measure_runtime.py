f2021f52091f7112f067f3e2385098896fe30d0b
isaac-svg <isaacsakyi009@gmail.com>
2024-05-07 11:54:41 +0000

    Task2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Executes async_comprehension four times in parallel using asyncio.gather().
+"""
+
+import asyncio
+from typing import List
+from time import perf_counter
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension four times in parallel using asyncio.gather().
+    Measures the total runtime and returns it.
+    """
+    start_time = perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = perf_counter()
+    return end_time - start_time
